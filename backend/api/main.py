fa3d762eaf9bdef5ef6de197fe87b2c68e8c9a67
pereedi <pereedi@gmail.com>
2026-01-22 03:14:42 -0800

    initial commit

--- backend/api/main.py
+++ backend/api/main.py
@@ -18,14 +18,7 @@ app.include_router(auth_router)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:5173",
-        "http://127.0.0.1:5173",
-        "http://localhost:5174",
-        "http://127.0.0.1:5174",
-        "http://localhost:5175",
-        "http://127.0.0.1:5175",
-    ],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
